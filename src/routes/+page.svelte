e7660b5ae623df939a9fd6f65f35205918eb5bad
Ivan Dustin Bilon <ivan22.dust@gmail.com>
2024-10-13 15:45:13 +0800

    Update search

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,16 +1,31 @@
-<main class="text-center flex items-center justify-center h-screen">
-	<div class="m-10 max-w-screen-md mt-[calc(61.8vh-254px/2)]">
-		<form class="border rounded-full border-gray-400 py-3 px-5 flex mb-16" action="results">
+<main class="text-center flex items-center justify-center h-screen bg-gray-50">
+	<div class="m-10 max-w-screen-md mt-[calc(61.8vh-224px/2)]">
+		<form class="rounded-full p-3 flex mb-16 shadow bg-white" action="results">
+			<svg
+				xmlns="http://www.w3.org/2000/svg"
+				class="h-6 w-6 text-gray-400"
+				fill="none"
+				viewBox="0 0 24 24"
+				stroke="currentColor"
+				stroke-width="2"
+			>
+				<path
+					stroke-linecap="round"
+					stroke-linejoin="round"
+					d="M21 21l-4.35-4.35m1.35-5.65a7 7 0 11-14 0 7 7 0 0114 0z"
+				/>
+			</svg>
 			<input
 				type="text"
-				class="flex-grow outline-none text-center placeholder:text-gray-300"
+				placeholder="Search"
 				enterkeyhint="search"
+				class="flex-grow outline-none bg-transparent placeholder:text-gray-300 mx-2"
 				autofocus
+				required
 			/>
 		</form>
 		<h2 class="text-xs w-3/5 m-auto leading-7 text-gray-400">
-			Search the Bible in your preferred language, and the results will be provided in the King
-			James Version (KJV).
+			Search the Bible in your language, and you'll get results in the King James Version (KJV).
 		</h2>
 	</div>
 </main>
