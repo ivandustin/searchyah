907a13997e5ce79dc8932777a2b7ddd44af2e7ab
Ivan Dustin Bilon <ivan22.dust@gmail.com>
2024-10-17 23:04:04 +0800

    Use nbsp

--- src/routes/[slug]/+page.svelte
+++ src/routes/[slug]/+page.svelte
@@ -5,7 +5,7 @@
 <main class="m-10 font-serif leading-10 text-2xl">
 	{#each data.records as record}
 		<p id={record.verse} class="my-10">
-			<sup class="font-sans text-xs select-none pr-2 text-gray-400">{record.verse}</sup
+			<sup class="font-sans text-xs select-none text-gray-400">{record.verse}&nbsp;</sup
 			>{record.text}
 		</p>
 	{/each}
