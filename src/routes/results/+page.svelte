2879eecbc2e3bd072b16234bb8bfd2f67fea1f88
Ivan Dustin Bilon <ivan22.dust@gmail.com>
2024-10-18 07:40:09 +0800

    Refactor results

--- src/routes/results/+page.svelte
+++ src/routes/results/+page.svelte
@@ -1,17 +1,14 @@
 <script>
-	import { toString } from '$lib/reference/verse/toString';
-	import { toSlug } from '$lib/reference/chapter/toSlug';
-	import { toText } from '$lib/reference/verse/toText';
 	export let data;
 </script>
 
 <main class="m-10">
-	{#each data.verses as verse}
+	{#each data.items as item}
 		<div class="my-20">
 			<p class="my-5 text-blue-500 tracking-widest">
-				<a href="/{toSlug(verse)}#{verse.verse + 1}">{toString(verse)}</a>
+				<a href={item.href}>{item.reference}</a>
 			</p>
-			<p class="font-serif leading-10 text-2xl">{toText(verse)}</p>
+			<p class="font-serif leading-10 text-2xl">{item.text}</p>
 		</div>
 	{/each}
 </main>
--- src/routes/results/+page.ts
+++ src/routes/results/+page.ts
@@ -1,10 +1,21 @@
+import { toString } from '$lib/reference/verse/toString';
+import { toSlug } from '$lib/reference/chapter/toSlug';
+import { toText } from '$lib/reference/verse/toText';
 import { get } from '$lib/reference/verse/get';
+import type { Verse } from '$lib/types/verse';
 
 export async function load({ fetch, url }) {
 	let query = url.searchParams.get('query');
 	if (query) {
 		const verses = await get(fetch, query);
-		return { verses };
+		const items = verses.map(function (verse: Verse) {
+			const text = toText(verse);
+			const reference = toString(verse);
+			const slug = toSlug(verse);
+			const href = `${slug}#${verse.verse + 1}`;
+			return { text, reference, href };
+		});
+		return { items };
 	}
 	throw new Error('Invalid query');
 }
