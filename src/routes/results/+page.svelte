1705037409e31954df8908a498c3ac4eabb97a89
Ivan Dustin Bilon <ivan22.dust@gmail.com>
2024-11-10 03:44:43 +0800

    Blue reference

--- src/routes/results/+page.svelte
+++ src/routes/results/+page.svelte
@@ -9,7 +9,7 @@
 <main class="m-10">
 	{#each data.items as item}
 		<a href={item.href} class="block my-20">
-			<p class="my-5 tracking-widest">
+			<p class="my-5 text-blue-500 tracking-widest">
 				{item.reference}
 			</p>
 			<p class="font-serif leading-10 text-2xl">{item.text}</p>
