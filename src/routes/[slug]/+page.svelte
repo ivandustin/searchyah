13034fc0c8fa6ad3cb0b5a217765664c55b603a2
Ivan Dustin Bilon <ivan22.dust@gmail.com>
2024-11-20 02:04:47 +0800

    Larger chapter heading

--- src/routes/[slug]/+page.svelte
+++ src/routes/[slug]/+page.svelte
@@ -7,16 +7,18 @@
 	<title>{data.title}</title>
 </svelte:head>
 
-<main class="m-5 font-serif leading-10 text-2xl mb-[38.2vh]">
-	<h1 class="p-5 font-sans text-xs text-gray-400 uppercase">{data.title}</h1>
-	{#each data.items as item}
-		<p
-			id={item.verse}
-			class="my-3 p-5 rounded-lg"
-			class:bg-gray-100={$page.url.hash == item.fragment}
-			class:dark:bg-gray-800={$page.url.hash == item.fragment}
-		>
-			<sup class="font-sans text-xs text-gray-400 select-none">{item.verse}&nbsp;</sup>{item.text}
-		</p>
-	{/each}
+<main class="m-5 mb-[38.2vh]">
+	<h1 class="p-5 text-gray-400 uppercase">{data.title}</h1>
+	<div class="font-serif leading-10 text-2xl">
+		{#each data.items as item}
+			<p
+				id={item.verse}
+				class="my-3 p-5 rounded-lg"
+				class:bg-gray-100={$page.url.hash == item.fragment}
+				class:dark:bg-gray-800={$page.url.hash == item.fragment}
+			>
+				<sup class="font-sans text-xs text-gray-400 select-none">{item.verse}&nbsp;</sup>{item.text}
+			</p>
+		{/each}
+	</div>
 </main>
