3af7ef4643a506f7a7aef528db09df3bf1e32df6
Ivan Dustin Bilon <ivan22.dust@gmail.com>
2026-02-22 19:58:01 +0800

    Update layout

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -15,72 +15,69 @@
 </svelte:head>
 
 <main class="mx-auto max-w-screen-md">
-	<div class="h-dvh flex flex-col justify-between text-center">
-		<div class="h-[61.8dvh] flex flex-col">
-			<div class="flex-auto"></div>
-			<div class="my-10">
-				<h1 class="font-serif text-4xl">Searchyah</h1>
-			</div>
-			<div class="mx-10">
-				<form
-					class="rounded-[calc(48px/1.618/1.618)] p-3 flex bg-gray-100 dark:bg-gray-700"
-					on:submit={onSubmit}
-					action="results"
-					method="GET"
-				>
-					<div class="h-6 w-6 relative">
-						<div class="absolute transition" class:opacity-0={submitted}>
-							<svg
-								xmlns="http://www.w3.org/2000/svg"
-								class="h-6 w-6 text-gray-500"
-								stroke="currentColor"
-								viewBox="0 0 24 24"
-								stroke-width="2"
-								fill="none"
-							>
-								<path
-									d="M21 21l-4.35-4.35m1.35-5.65a7 7 0 11-14 0 7 7 0 0114 0z"
-									stroke-linejoin="round"
-									stroke-linecap="round"
-								/>
-							</svg>
-						</div>
-						<div class="absolute transition" class:opacity-0={!submitted}>
-							<svg
-								class="h-6 w-6 text-gray-300 animate-spin dark:text-gray-500 fill-blue-500 mx-auto"
-								xmlns="http://www.w3.org/2000/svg"
-								class:hidden={!submitted}
-								viewBox="0 0 100 101"
-								aria-hidden="true"
-								fill="none"
-							>
-								<path
-									d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
-									fill="currentColor"
-								/>
-								<path
-									d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
-									fill="currentFill"
-								/>
-							</svg>
-						</div>
-					</div>
-					<input
-						class="flex-grow outline-none bg-transparent placeholder:text-gray-500 mx-3"
-						placeholder="bible search"
-						enterkeyhint="search"
-						autocapitalize="off"
-						readonly={submitted}
-						type="search"
-						name="query"
-						autofocus
-						required
-					/>
-				</form>
-			</div>
+	<div class="h-dvh text-center flex flex-col">
+		<div class="h-[76.4dvh] flex flex-col justify-center">
+			<h1 class="font-serif text-4xl">Searchyah</h1>
+			<p class="m-10 text-xs text-gray-500">
+				{description}
+			</p>
 		</div>
-		<p class="m-8 text-xs text-gray-500">
-			{description}
-		</p>
+		<div class="mx-10 -translate-y-1/2">
+			<form
+				class="rounded-[calc(48px/1.618/1.618)] p-3 flex bg-gray-100 dark:bg-gray-700"
+				on:submit={onSubmit}
+				action="results"
+				method="GET"
+			>
+				<div class="h-6 w-6 relative">
+					<div class="absolute transition" class:opacity-0={submitted}>
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							class="h-6 w-6 text-gray-500"
+							stroke="currentColor"
+							viewBox="0 0 24 24"
+							stroke-width="2"
+							fill="none"
+						>
+							<path
+								d="M21 21l-4.35-4.35m1.35-5.65a7 7 0 11-14 0 7 7 0 0114 0z"
+								stroke-linejoin="round"
+								stroke-linecap="round"
+							/>
+						</svg>
+					</div>
+					<div class="absolute transition" class:opacity-0={!submitted}>
+						<svg
+							class="h-6 w-6 text-gray-300 animate-spin dark:text-gray-500 fill-blue-500 mx-auto"
+							xmlns="http://www.w3.org/2000/svg"
+							class:hidden={!submitted}
+							viewBox="0 0 100 101"
+							aria-hidden="true"
+							fill="none"
+						>
+							<path
+								d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+								fill="currentColor"
+							/>
+							<path
+								d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+								fill="currentFill"
+							/>
+						</svg>
+					</div>
+				</div>
+				<input
+					class="flex-grow outline-none bg-transparent placeholder:text-gray-500 mx-3"
+					placeholder="bible search"
+					enterkeyhint="search"
+					autocapitalize="off"
+					readonly={submitted}
+					type="search"
+					name="query"
+					autofocus
+					required
+				/>
+			</form>
+		</div>
 	</div>
 </main>
